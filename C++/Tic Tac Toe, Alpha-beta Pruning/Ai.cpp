c801d5f0af65a26a3a0276b04ab28ed4d5521d47
Kevin <kevinjasonhol@gmail.com>
2014-06-24 10:48:14 +0100

    update
    optimized and fixed alpha beta pruning

--- C++/Tic Tac Toe, Alpha-beta Pruning/Ai.cpp
+++ C++/Tic Tac Toe, Alpha-beta Pruning/Ai.cpp
@@ -9,42 +9,41 @@ Ai::Ai(){
 
 int Engine::getMove(char board[3][3]){
     int score = -10000;
-    for (int i=0; i<3;i++){
-        char tempBoard[3][3];
-        tempBoard = copyBoard(board);
-        for(int j=0;j<3;j++){
-            if(board[i][j]!='X'&&board[i][j]!='O'){//For every empty position
-                tempBoard[i][j] = aiPlayer;
-                score = max(score,getMin(tempBoard,alpha,beta));
-                if(score>=beta){return (i+(j*3))}; //Prune tree.
-                alpha = max(alpha,score);
-            }
-        }
-    }
-    return i+(j*3);
+    int move = NULL;
+    score = max(score,getMin(board,-10000,10000,&move));
+    return move;
 }
 
 
 
-int Engine::getMax(char board[3][3], int alpha, int beta){
-    int score = -10000;
+int Engine::getMax(char board[3][3], int alpha, int beta, int* move = NULL){
+    
+    
+    int score = 0;
+    int bestscore = -10000;
     for (int i=0; i<3;i++){
         char tempBoard[3][3];
         tempBoard = copyBoard(board);
         for(int j=0;j<3;j++){
             if(board[i][j]!='X'&&board[i][j]!='O'){//For every empty position
                 tempBoard[i][j] = aiPlayer;
-                score = max(score,getMin(tempBoard,alpha,beta));
+                score = getMin(tempBoard,alpha,beta);
+                if(score>= bestscore){ //get Max
+                    bestscore = score;
+                    move = ((i*3)+j);
+                }
                 if(score>=beta){return score;}; //Prune tree.
                 alpha = max(alpha,score);
             }
         }
     }
-    return score;
+    //Full board past this
+    return getScore(board);
 }
 
-int Engine::getMin(char board[3][3], int alpha, int beta){
-    int score = 10000;
+int Engine::getMin(char board[3][3], int alpha, int beta, int* move = NULL){
+    int score = 0;
+    int bestscore = 10000
     for (int i=0; i<3;i++){
         char tempBoard[3][3];
         int tempScore = 0;
@@ -52,13 +51,18 @@ int Engine::getMin(char board[3][3], int alpha, int beta){
         for(int j=0;j<3;j++){
             if(board[i][j]!='X'&&board[i][j]!='O'){//For every empty position
                 tempBoard[i][j] = huPlayer;
-                score = min(score,getMax(tempBoard,alpha,beta));
-                if(score<=alpha){return score;}; //Prune tree.
-                alpha = min(beta,score);
+                score = getMax(tempBoard,alpha,beta);
+                if(score<=bestscore){ //get Min
+                    bestscore = score;
+                    move = ((i*3)+j);
+                }
+                if(score<=alpha){return bestscore;}; //Prune tree.
+                alpha = min(beta,bestscore);
             }
         }
     }
-    return score;
+    //Full board past this
+    return getScore(board);
 }
 
 int Engine::getScore(char board[3][3]){ //Get heuristic,  +10 for 3 Xs in a line, +1 for 2 Xs in a line, -10 for 3 Os in a line, -1 for 2 Os in a line. 
